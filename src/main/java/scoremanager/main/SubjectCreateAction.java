c6ecf88b9e7c4fce87e033fa289a200afdf5bdfd
'itan' <'itan@itan.com'>
2026-04-24 09:31:20 +0900

    subjectCreateaction仮完成　堀江

--- src/main/java/scoremanager/main/SubjectCreateAction.java
+++ src/main/java/scoremanager/main/SubjectCreateAction.java
@@ -1,13 +1,7 @@
 package scoremanager.main;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-
-import dao.ClassNumDao;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
 import tool.Action;
 
 public class SubjectCreateAction extends Action {
@@ -15,23 +9,7 @@ public class SubjectCreateAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		// TODO 自動生成されたメソッド・スタブ
-		HttpSession session = req.getSession(); // セッション
-
-		LocalDate todaysDate = LocalDate.now(); // LocalDateインスタンスを取得
-		int year = todaysDate.getYear(); // 現在の年を取得
-		ClassNumDao cNumDao = new ClassNumDao(); // クラス番号Daoを初期化
-
-		// リストを初期化
-		List<Integer> entYearSet = new ArrayList<>();
-		// 10年前から1年後まで年をリストに追加
-		for (int i = year - 10; i < year + 1; i++) {
-			entYearSet.add(i);
-		}
-
-		// DBからデータ取得
-
-		req.setAttribute("ent_year_set", entYearSet);
-
+		
 		// JSPへフォワード 7
 		req.getRequestDispatcher("student_create.jsp").forward(req, res);
 
